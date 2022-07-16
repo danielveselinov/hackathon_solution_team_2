3bb2692c321f88e6f851de95a2d3a5f17fb4ed6d
Atanasija Gjorgievski <gjorgievskiatanasija@gmail.com>
2022-07-16 15:59:30 +0200

    Services READ and APi endpoint + Banner Image Model and Migration

--- app/Http/Controllers/Api/ServicesController.php
+++ app/Http/Controllers/Api/ServicesController.php
@@ -5,12 +5,22 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\ServicesCollection;
 use App\Models\Service;
+use App\Models\ServiceType;
 use Illuminate\Http\Request;
 
 class ServicesController extends Controller
 {
     public function index() 
     {
-        return new ServicesCollection(Service::all());
+        $types = ServiceType::all();
+
+        $array = [];
+
+        foreach ($types as $type) {
+            $allBlogs = Service::where('service_type_id', $type->id)->get();
+            $service = new ServicesCollection($allBlogs);
+            $array[strtolower(str_replace(' ', '_', $type->name))] = $service;
+        }
+        return $array;
     }
 }
