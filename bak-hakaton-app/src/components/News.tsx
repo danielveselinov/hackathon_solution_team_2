afc623498e0a8405c3179bb32f40aa8349a04017
dtodorovski24 <todorovskidavid3@gmail.com>
2022-07-17 18:15:40 +0200

    post request

--- bak-hakaton-app/src/components/News.tsx
+++ bak-hakaton-app/src/components/News.tsx
@@ -1,4 +1,5 @@
 /* eslint-disable @next/next/no-img-element */
+import Link from "next/link";
 import React from "react";
 import { Card, CardGroup, Carousel, Col, Row } from "react-bootstrap";
 // import styles from "../styles/news.module.scss";
@@ -16,44 +17,57 @@ export default function News() {
               className={`${styles.RowCustomWidth} m-auto justify-content-between `}
             >
               <Card className="col-lg-3 bg-transparent border-0">
-                <Card.Img
-                  variant="top"
-                  src="https://i.picsum.photos/id/195/200/300.jpg?hmac=4jGQkshsI0i2q2zt0L5AnB3c8yyqVBkmkYR0zDKIpRQ"
-                />
-                <Card.Body>
-                  <Card.Title>Card title</Card.Title>
-                  <Card.Text>
-                    This is a wider card with supporting text below as a natural
-                    lead-in to additional content.
-                  </Card.Text>
-                </Card.Body>
+                <Link href="/">
+                  <a>
+                    <Card.Img
+                      variant="top"
+                      src="https://i.picsum.photos/id/195/200/300.jpg?hmac=4jGQkshsI0i2q2zt0L5AnB3c8yyqVBkmkYR0zDKIpRQ"
+                    />
+                    <Card.Body>
+                      <Card.Title>Card title</Card.Title>
+                      <Card.Text>
+                        This is a wider card with supporting text below as a
+                        natural lead-in to additional content.
+                      </Card.Text>
+                    </Card.Body>
+                  </a>
+                </Link>
               </Card>
               <Card className="col-lg-3 bg-transparent border-0">
-                <Card.Img
-                  variant="top"
-                  src="https://i.picsum.photos/id/195/200/300.jpg?hmac=4jGQkshsI0i2q2zt0L5AnB3c8yyqVBkmkYR0zDKIpRQ"
-                />
-                <Card.Body>
-                  <Card.Title>Card title</Card.Title>
-                  <Card.Text>
-                    This card has supporting text below as a natural lead-in to
-                    additional content.{" "}
-                  </Card.Text>
-                </Card.Body>
+                <Link href="/">
+                  <a>
+                    <Card.Img
+                      variant="top"
+                      src="https://i.picsum.photos/id/195/200/300.jpg?hmac=4jGQkshsI0i2q2zt0L5AnB3c8yyqVBkmkYR0zDKIpRQ"
+                    />
+                    <Card.Body>
+                      <Card.Title>Card title</Card.Title>
+                      <Card.Text>
+                        This card has supporting text below as a natural lead-in
+                        to additional content.{" "}
+                      </Card.Text>
+                    </Card.Body>
+                  </a>
+                </Link>
               </Card>
               <Card className="col-lg-3  bg-transparent border-0">
-                <Card.Img
-                  variant="top"
-                  src="https://i.picsum.photos/id/195/200/300.jpg?hmac=4jGQkshsI0i2q2zt0L5AnB3c8yyqVBkmkYR0zDKIpRQ"
-                />
-                <Card.Body>
-                  <Card.Title>Card title</Card.Title>
-                  <Card.Text>
-                    This is a wider card with supporting text below as a natural
-                    lead-in to additional content. This card has even longer
-                    content than the first to show that equal height action.
-                  </Card.Text>
-                </Card.Body>
+                <Link href="/">
+                  <a>
+                    <Card.Img
+                      variant="top"
+                      src="https://i.picsum.photos/id/195/200/300.jpg?hmac=4jGQkshsI0i2q2zt0L5AnB3c8yyqVBkmkYR0zDKIpRQ"
+                    />
+                    <Card.Body>
+                      <Card.Title>Card title</Card.Title>
+                      <Card.Text>
+                        This is a wider card with supporting text below as a
+                        natural lead-in to additional content. This card has
+                        even longer content than the first to show that equal
+                        height action.
+                      </Card.Text>
+                    </Card.Body>
+                  </a>
+                </Link>
               </Card>
             </Row>
           </Carousel.Item>
@@ -62,45 +76,58 @@ export default function News() {
               className={`${styles.RowCustomWidth} m-auto justify-content-between`}
             >
               <Card className="col-3 bg-transparent border-0">
-                <Card.Img
-                  variant="top"
-                  src="https://i.picsum.photos/id/195/200/300.jpg?hmac=4jGQkshsI0i2q2zt0L5AnB3c8yyqVBkmkYR0zDKIpRQ"
-                />
-                <Card.Body>
-                  <Card.Title>Card title</Card.Title>
-                  <Card.Text>
-                    This is a wider card with supporting text below as a natural
-                    lead-in to additional content. This content is a little bit
-                    longer.
-                  </Card.Text>
-                </Card.Body>
+                <Link href="/">
+                  <a>
+                    <Card.Img
+                      variant="top"
+                      src="https://i.picsum.photos/id/195/200/300.jpg?hmac=4jGQkshsI0i2q2zt0L5AnB3c8yyqVBkmkYR0zDKIpRQ"
+                    />
+                    <Card.Body>
+                      <Card.Title>Card title</Card.Title>
+                      <Card.Text>
+                        This is a wider card with supporting text below as a
+                        natural lead-in to additional content. This content is a
+                        little bit longer.
+                      </Card.Text>
+                    </Card.Body>
+                  </a>
+                </Link>
               </Card>
               <Card className="col-3 bg-transparent border-0">
-                <Card.Img
-                  variant="top"
-                  src="https://i.picsum.photos/id/195/200/300.jpg?hmac=4jGQkshsI0i2q2zt0L5AnB3c8yyqVBkmkYR0zDKIpRQ"
-                />
-                <Card.Body>
-                  <Card.Title>Card title</Card.Title>
-                  <Card.Text>
-                    This card has supporting text below as a natural lead-in to
-                    additional content.{" "}
-                  </Card.Text>
-                </Card.Body>
+                <Link href="/">
+                  <a>
+                    <Card.Img
+                      variant="top"
+                      src="https://i.picsum.photos/id/195/200/300.jpg?hmac=4jGQkshsI0i2q2zt0L5AnB3c8yyqVBkmkYR0zDKIpRQ"
+                    />
+                    <Card.Body>
+                      <Card.Title>Card title</Card.Title>
+                      <Card.Text>
+                        This card has supporting text below as a natural lead-in
+                        to additional content.{" "}
+                      </Card.Text>
+                    </Card.Body>
+                  </a>
+                </Link>
               </Card>
               <Card className="col-3 bg-transparent border-0">
-                <Card.Img
-                  variant="top"
-                  src="https://i.picsum.photos/id/195/200/300.jpg?hmac=4jGQkshsI0i2q2zt0L5AnB3c8yyqVBkmkYR0zDKIpRQ"
-                />
-                <Card.Body>
-                  <Card.Title>Card title</Card.Title>
-                  <Card.Text>
-                    This is a wider card with supporting text below as a natural
-                    lead-in to additional content. This card has even longer
-                    content than the first to show that equal height action.
-                  </Card.Text>
-                </Card.Body>
+                <Link href="/">
+                  <a>
+                    <Card.Img
+                      variant="top"
+                      src="https://i.picsum.photos/id/195/200/300.jpg?hmac=4jGQkshsI0i2q2zt0L5AnB3c8yyqVBkmkYR0zDKIpRQ"
+                    />
+                    <Card.Body>
+                      <Card.Title>Card title</Card.Title>
+                      <Card.Text>
+                        This is a wider card with supporting text below as a
+                        natural lead-in to additional content. This card has
+                        even longer content than the first to show that equal
+                        height action.
+                      </Card.Text>
+                    </Card.Body>
+                  </a>
+                </Link>
               </Card>
             </Row>
           </Carousel.Item>
@@ -109,45 +136,58 @@ export default function News() {
               className={`${styles.RowCustomWidth} m-auto justify-content-between`}
             >
               <Card className="col-3 bg-transparent border-0">
-                <Card.Img
-                  variant="top"
-                  src="https://i.picsum.photos/id/195/200/300.jpg?hmac=4jGQkshsI0i2q2zt0L5AnB3c8yyqVBkmkYR0zDKIpRQ"
-                />
-                <Card.Body>
-                  <Card.Title>Card title</Card.Title>
-                  <Card.Text>
-                    This is a wider card with supporting text below as a natural
-                    lead-in to additional content. This content is a little bit
-                    longer.
-                  </Card.Text>
-                </Card.Body>
+                <Link href="/">
+                  <a>
+                    <Card.Img
+                      variant="top"
+                      src="https://i.picsum.photos/id/195/200/300.jpg?hmac=4jGQkshsI0i2q2zt0L5AnB3c8yyqVBkmkYR0zDKIpRQ"
+                    />
+                    <Card.Body>
+                      <Card.Title>Card title</Card.Title>
+                      <Card.Text>
+                        This is a wider card with supporting text below as a
+                        natural lead-in to additional content. This content is a
+                        little bit longer.
+                      </Card.Text>
+                    </Card.Body>
+                  </a>
+                </Link>
               </Card>
               <Card className="col-3 bg-transparent border-0">
-                <Card.Img
-                  variant="top"
-                  src="https://i.picsum.photos/id/195/200/300.jpg?hmac=4jGQkshsI0i2q2zt0L5AnB3c8yyqVBkmkYR0zDKIpRQ"
-                />
-                <Card.Body>
-                  <Card.Title>Card title</Card.Title>
-                  <Card.Text>
-                    This card has supporting text below as a natural lead-in to
-                    additional content.{" "}
-                  </Card.Text>
-                </Card.Body>
+                <Link href="/">
+                  <a>
+                    <Card.Img
+                      variant="top"
+                      src="https://i.picsum.photos/id/195/200/300.jpg?hmac=4jGQkshsI0i2q2zt0L5AnB3c8yyqVBkmkYR0zDKIpRQ"
+                    />
+                    <Card.Body>
+                      <Card.Title>Card title</Card.Title>
+                      <Card.Text>
+                        This card has supporting text below as a natural lead-in
+                        to additional content.{" "}
+                      </Card.Text>
+                    </Card.Body>
+                  </a>
+                </Link>
               </Card>
               <Card className="col-3 bg-transparent border-0">
-                <Card.Img
-                  variant="top"
-                  src="https://i.picsum.photos/id/195/200/300.jpg?hmac=4jGQkshsI0i2q2zt0L5AnB3c8yyqVBkmkYR0zDKIpRQ"
-                />
-                <Card.Body>
-                  <Card.Title>Card title</Card.Title>
-                  <Card.Text>
-                    This is a wider card with supporting text below as a natural
-                    lead-in to additional content. This card has even longer
-                    content than the first to show that equal height action.
-                  </Card.Text>
-                </Card.Body>
+                <Link href="/">
+                  <a>
+                    <Card.Img
+                      variant="top"
+                      src="https://i.picsum.photos/id/195/200/300.jpg?hmac=4jGQkshsI0i2q2zt0L5AnB3c8yyqVBkmkYR0zDKIpRQ"
+                    />
+                    <Card.Body>
+                      <Card.Title>Card title</Card.Title>
+                      <Card.Text>
+                        This is a wider card with supporting text below as a
+                        natural lead-in to additional content. This card has
+                        even longer content than the first to show that equal
+                        height action.
+                      </Card.Text>
+                    </Card.Body>
+                  </a>
+                </Link>
               </Card>
             </Row>
           </Carousel.Item>
