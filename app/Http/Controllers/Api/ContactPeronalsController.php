b05a471e8ca1700a4321b9392e38c44621812cc1
Daniel Veselinov <daniel@veselinov.com>
2022-07-17 19:37:47 +0200

    Sending email when registnig in FORM

--- app/Http/Controllers/Api/ContactPeronalsController.php
+++ app/Http/Controllers/Api/ContactPeronalsController.php
@@ -5,12 +5,19 @@ namespace App\Http\Controllers\Api;
 use Illuminate\Http\Request;
 use App\Models\ContactPersonal;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Mail;
 use App\Http\Requests\ContactPersonalsStoreRequest;
 
 class ContactPeronalsController extends Controller
 {
     public function store(ContactPersonalsStoreRequest $request)
     {
+
+        $data = [
+            'email' => $request->email,
+        ];
+
+
         $contact_personals = ContactPersonal::create([
             'full_name' => $request->full_name,
             'email' => $request->email,
@@ -20,6 +27,12 @@ class ContactPeronalsController extends Controller
         if (!$contact_personals) {
             return response()->json(['message' => 'Error', 'code' => 400]);
         }
+
+        Mail::send('templates/mail', $data, function($message) use ($data) {
+            $message->to($data['email'])
+                    ->subject('Welcome to Bak Logistics')
+                    ->from('info@example.com', 'Bak Logistics Hiring');
+        });
 
         return response()->json(['message' => 'Successfully submited', 'code' => 200]);
     }
---
+++ resources/views/templates/mail.blade.php
@@ -0,0 +1,6 @@
+Hello {{ $email }},<br><br>
+
+Thank you for your interest.<br>
+
+Kindly,<br>
+Bak Logistics.
